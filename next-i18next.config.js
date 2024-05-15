33507b246c6cf7c33c630ddfe363fcf5dc262be1
Marcos Javier Martínez Ramos <maglor_elf@yahoo.es>
2024-05-15 21:02:54 +0200

    feat: Locales changes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE'],
+    defaultLocale: 'es-ES',
+    locales: ['es-ES','en-GB'],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
